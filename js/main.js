904155ec371e44d10a627c768429c8b565f91b48
Yoksan Herlie <yoksanh@gmail.com>
2019-07-25 07:29:59 +0700

    add part 2d and some animations

--- js/main.js
+++ js/main.js
@@ -645,6 +645,308 @@ function loadScene1c4() {
     ]);
 }
 
+// scene 2 a
+function loadScene2d1() {
+    let textScene = new ScrollMagic.Scene({
+        triggerElement: '#scene-2-d-1',
+    })
+        .setClassToggle('.text-d1-1', 'reveal').addIndicators();
+
+    let grass = new ScrollMagic.Scene({
+        triggerElement: '#scene-2-d-1',
+    }).setVelocity('.grass-svg', { opacity: 1, top: 0 }, { duration: 750 }).addIndicators();
+
+    controller.addScene([
+        textScene, grass
+    ]);
+}
+
+function loadScene2d2() {
+    let castle = new ScrollMagic.Scene({
+        triggerElement: '#scene-2-d-2',
+    })
+        .setVelocity('.castle-svg', { opacity: 1, top: 0 }, { duration: 300 }).addIndicators();
+
+    let cloud1 = new ScrollMagic.Scene({
+        triggerElement: '#scene-2-d-2',
+        offset: 200
+    })
+        .setVelocity('.castle-cloud-1', { opacity: 1, left: -20 }, { duration: 500 }).addIndicators();
+
+    let cloud2 = new ScrollMagic.Scene({
+        triggerElement: '#scene-2-d-2',
+        offset: 200
+    })
+        .setVelocity('.castle-cloud-2', { opacity: 1, right: 50 }, { duration: 500 }).addIndicators();
+
+    controller.addScene([
+        castle, cloud1, cloud2
+    ]);
+}
+
+function loadScene2d3() {
+    let block1 = new ScrollMagic.Scene({
+        triggerElement: '#scene-2-d-3',
+    })
+        .setVelocity('.scene-block-2d3-1 .block', { bottom: 0, opacity: 1 }, { duration: 500 })
+        .addIndicators();
+
+    let block2 = new ScrollMagic.Scene({
+        triggerElement: '#scene-2-d-3',
+    })
+        .setVelocity('.scene-block-2d3-2 .block-2d3-2-1', { top: 0, opacity: 1 }, { duration: 500, delay: 250 })
+        .addIndicators();
+
+    let block3 = new ScrollMagic.Scene({
+        triggerElement: '#scene-2-d-3',
+    })
+        .setVelocity('.scene-block-2d3-2 .block-2d3-2-2', { right: 0, opacity: 1 }, { duration: 500, delay: 400 })
+        .addIndicators();
+
+    let chat1 = new ScrollMagic.Scene({
+        triggerElement: '#scene-2-d-3',
+        offset: 100
+    })
+        .setClassToggle('.chat-2d3-1', 'animate-chat')
+        .addIndicators();
+
+    let chat2 = new ScrollMagic.Scene({
+        triggerElement: '#scene-2-d-3',
+        offset: 200
+    })
+        .setClassToggle('.chat-2d3-2', 'animate-chat')
+        .addIndicators();
+
+    let chat3 = new ScrollMagic.Scene({
+        triggerElement: '#scene-2-d-3',
+        offset: 300
+    })
+        .setClassToggle('.chat-2d3-3', 'animate-chat')
+        .addIndicators();
+
+    let text1 = new ScrollMagic.Scene({
+        triggerElement: '#scene-2-d-3',
+        offset: 400
+    })
+        .setVelocity('.scene-block-2d3-2 .block-text', { opacity: 1 })
+        .addIndicators();
+
+    controller.addScene([
+        block1, block2, block3, chat1, chat2, chat3, text1
+    ])
+}
+
+function loadScene2d4() {
+    let text1 = new ScrollMagic.Scene({
+        triggerElement: '#scene-2-d-4',
+    })
+        .setVelocity('.scene-block-2d4 .block-text-1', { opacity: 1 })
+        .addIndicators();
+
+    let block1 = new ScrollMagic.Scene({
+        triggerElement: '#scene-2-d-4',
+    })
+        .setVelocity('.scene-block-2d4-1 .block-2d4-1-1', { bottom: 0, opacity: 1 }, { duration: 500 })
+        .addIndicators();
+
+    let block2 = new ScrollMagic.Scene({
+        triggerElement: '#scene-2-d-4',
+    })
+        .setVelocity('.scene-block-2d4-1 .block-2d4-1-2', { bottom: 40, opacity: 1 }, { duration: 500, delay: 250 })
+        .addIndicators();
+
+    let text2 = new ScrollMagic.Scene({
+        triggerElement: '#scene-2-d-4',
+        offset: 100
+    })
+        .setVelocity('.scene-block-2d4 .block-text-2', { opacity: 1 })
+        .addIndicators();
+
+    let block3 = new ScrollMagic.Scene({
+        triggerElement: '#scene-2-d-4',
+        offset: 200
+    })
+        .setVelocity('.scene-block-2d4-2 .block-2d4-1-3', { left: 10, opacity: 1 }, { duration: 750 })
+        .addIndicators();
+
+    let chat1 = new ScrollMagic.Scene({
+        triggerElement: '#scene-2-d-4',
+        offset: 300
+    })
+        .setClassToggle('.scene-block-2d4-2 .chat-cloud-new', 'animate-chat')
+        .addIndicators();
+
+    controller.addScene([
+        text1, block1, block2, text2, block3, chat1
+    ]);
+}
+
+function loadScene2d5() {
+    let block1 = new ScrollMagic.Scene({
+        triggerElement: '#scene-2-d-5',
+    })
+        .setVelocity('.scene-block-2d5-1 .block-2d5-1', { bottom: 0, opacity: 1 }, { duration: 500 })
+        .addIndicators();
+
+    let block2 = new ScrollMagic.Scene({
+        triggerElement: '#scene-2-d-5',
+    })
+        .setVelocity('.scene-block-2d5-1 .block-2d5-2', { bottom: 0, opacity: 1 }, { duration: 500, delay: 100 })
+        .addIndicators();
+
+    let block3 = new ScrollMagic.Scene({
+        triggerElement: '#scene-2-d-5',
+    })
+        .setVelocity('.scene-block-2d5-1 .block-2d5-3', { bottom: 0, opacity: 1 }, { duration: 500, delay: 200 })
+        .addIndicators();
+
+    let chat1 = new ScrollMagic.Scene({
+        triggerElement: '#scene-2-d-5',
+        offset: 200
+    })
+        .setClassToggle('.scene-block-2d5-1 .block-2d5-1 .chat-cloud-new', 'animate-chat')
+        .addIndicators();
+
+    let chat2 = new ScrollMagic.Scene({
+        triggerElement: '#scene-2-d-5',
+        offset: 200
+    })
+        .setClassToggle('.scene-block-2d5-1 .block-2d5-2 .chat-cloud-new', 'animate-chat')
+        .addIndicators();
+
+    let block4 = new ScrollMagic.Scene({
+        triggerElement: '#scene-2-d-5',
+        offset: 300
+    })
+        .setVelocity('.scene-block-2d5-2', { left: -115, opacity: 1 }, { duration: 750 })
+        .addIndicators();
+
+    let text1 = new ScrollMagic.Scene({
+        triggerElement: '#scene-2-d-5',
+        offset: 300
+    })
+        .setVelocity('.scene-block-2d5-3', { opacity: 1 }, { duration: 500, delay: 300 })
+        .addIndicators();
+
+    controller.addScene([
+        block1, block2, block3, chat1, chat2, block4, text1
+    ])
+}
+
+function loadScene2d6() {
+    let block1 = new ScrollMagic.Scene({
+        triggerElement: '#scene-2-d-6',
+    })
+        .setVelocity('.scene-block-2d6-1', { bottom: 0, opacity: 1 }, { duration: 500 })
+        .addIndicators();
+
+    let block2 = new ScrollMagic.Scene({
+        triggerElement: '#scene-2-d-6',
+    })
+        .setVelocity('.scene-block-2d6-2 .scene-block-2d6-2-1 .block', { top: 0, opacity: 1 }, { duration: 500 })
+        .addIndicators();
+
+    let text1 = new ScrollMagic.Scene({
+        triggerElement: '#scene-2-d-6',
+        offset: 100
+    })
+        .setVelocity('.scene-block-2d6-2 .scene-block-2d6-2-1 .block-text', { opacity: 1 }, { duration: 500, delay: 200 })
+        .addIndicators();
+
+    let block3 = new ScrollMagic.Scene({
+        triggerElement: '#scene-2-d-6',
+        offset: 150
+    })
+        .setVelocity('.scene-block-2d6-2 .block-flex-1', { bottom: 0, opacity: 1 }, { duration: 500 })
+        .addIndicators();
+
+    let block4 = new ScrollMagic.Scene({
+        triggerElement: '#scene-2-d-6',
+        offset: 150
+    })
+        .setVelocity('.scene-block-2d6-2 .block-flex-2', { right: 0, opacity: 1 }, { duration: 500 })
+        .addIndicators();
+
+    let text2 = new ScrollMagic.Scene({
+        triggerElement: '#scene-2-d-6',
+        offset: 150
+    })
+        .setVelocity('.scene-block-2d6-2 .scene-block-2d6-2-2 .block-text', { opacity: 1 }, { duration: 500 })
+        .addIndicators();
+
+    let chat1 = new ScrollMagic.Scene({
+        triggerElement: '#scene-2-d-6',
+        offset: 50
+    })
+        .setClassToggle('.ccn-2d6-1', 'animate-chat')
+        .addIndicators();
+
+    let chat2 = new ScrollMagic.Scene({
+        triggerElement: '#scene-2-d-6',
+        offset: 100
+    })
+        .setClassToggle('.ccn-2d6-2', 'animate-chat')
+        .addIndicators();
+
+    let chat3 = new ScrollMagic.Scene({
+        triggerElement: '#scene-2-d-6',
+        offset: 150
+    })
+        .setClassToggle('.ccn-2d6-3', 'animate-chat')
+        .addIndicators();
+
+    let chat4 = new ScrollMagic.Scene({
+        triggerElement: '#scene-2-d-6',
+        offset: 200
+    })
+        .setClassToggle('.ccn-2d6-4', 'animate-chat')
+        .addIndicators();
+
+    controller.addScene([
+        block1, block2, text1, block3, block4, text2, chat1, chat2, chat3, chat4
+    ])
+}
+
+function loadScene2d7() {
+    let text1 = new ScrollMagic.Scene({
+        triggerElement: '#scene-2-d-7',
+        offset: 200
+    })
+        .setVelocity('.text-d7-1', { opacity: 1, top: 300 }, { duration: 500 }).addIndicators();
+
+    let textScene = new ScrollMagic.Scene({
+        triggerElement: '#scene-2-d-7',
+        offset: 250
+    })
+        .setClassToggle('.text-d7-2', 'reveal').addIndicators();
+
+    controller.addScene([
+        text1, textScene
+    ])
+}
+
+function loadScene2d8() {
+    let cloud1 = new ScrollMagic.Scene({
+        triggerElement: '#scene-2-d-8',
+    })
+        .setVelocity('.cloud-end-right', { opacity: 1, right: 0 }, { duration: 500 }).addIndicators();
+
+    let cloud2 = new ScrollMagic.Scene({
+        triggerElement: '#scene-2-d-8',
+    })
+        .setVelocity('.cloud-end-left', { opacity: 1, left: 0 }, { duration: 500 }).addIndicators();
+
+    let textEnd = new ScrollMagic.Scene({
+        triggerElement: '#scene-2-d-8',
+        offset: 300
+    })
+        .setClassToggle('.next-title', 'reveal').addIndicators();
+
+    controller.addScene([
+        cloud1, cloud2, textEnd
+    ])
+}
+
 // parallax
 function loadParallax() {
     let parallax1 = document.getElementById('parallax-1');
@@ -662,16 +964,20 @@ function loadParallax() {
         relativeInput: true
     });
 
+    let title3 = document.getElementById('ganteng3');
+    new Parallax(title3, {
+        relativeInput: true
+    });
 
     let sceneCloud = document.getElementById('cloud-ganteng');
     new Parallax(sceneCloud, {
         relativeInput: true
     });
 
-    let scene1c1 = document.getElementById('scene-1-c-1');
+    /*let scene1c1 = document.getElementById('scene-1-c-1');
     new Parallax(scene1c1, {
         relativeInput: true
-    });
+    });*/
 }
 
 function setupMusic() {
@@ -679,6 +985,8 @@ function setupMusic() {
 
     audioBg.loop = true;
     audioBg.volume = .5;
+
+    window.scrollTo(0, 0);
 }
 
 window.onload = () => {
@@ -700,6 +1008,15 @@ window.onload = () => {
     loadScene1c2();
     loadScene1c3();
     loadScene1c4();
+
+    loadScene2d1();
+    loadScene2d2();
+    loadScene2d3();
+    loadScene2d4();
+    loadScene2d5();
+    loadScene2d6();
+    loadScene2d7();
+    loadScene2d8();
 
     // loadParallax();
     setupMusic();
